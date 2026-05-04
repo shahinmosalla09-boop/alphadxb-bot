e1927aff99b4c4166685b28bcd59c7b84b6ed048
shahinmosalla09-boop <shahinmosalla09@gmail.com>
2026-05-04 08:59:55 +0400

    Update alphadxb_bot.py

--- alphadxb_bot.py
+++ alphadxb_bot.py
@@ -10,7 +10,7 @@ from datetime import datetime
 from io import BytesIO
 
 TELEGRAM_TOKEN = "8774593158:AAEtqggo7ReinWqO9rkUw6v74jA9HCEJcA4"
-ANTHROPIC_API_KEY = os.environ.get("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", "")
+ANTHROPIC_API_KEY = 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
 PUBLIC_CHANNEL = "@AlphaDXBcrypto"
 VIP_CHANNEL = "@AlphaDXBcryptoPRO"
 
